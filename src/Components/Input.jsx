3c0587116bd2bbbf83e2a39cd10a33b72d40957f
kaankacar <kaankacar02@gmail.com>
2022-10-20 23:58:01 +0300

    all necessary things coded

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { UilSearch,UilLocationPoint  } from '@iconscout/react-unicons'
 
-function Input() {
+function Input({setQuery,units,setUnits}) {
+  const [city,setCity] = useState("");
+  const handleSearchClick = () => {
+    if (city !=="") setQuery({q:city})
+  }
+  const handleLocationClick = () => {
+    if (navigator.geolocation){
+      navigator.geolocation.getCurrentPosition((position) => {
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+        setQuery({lat,lon})
+      })
+    }
+  }
+  const handleUnitsChange = (e) => {
+    const selectedUnit = e.currentTarget.name;
+    if (units !== selectedUnit) setUnits(selectedUnit);
+  }
   return (
     <div className='flex flex-row justify-center my-6'>
         <div className='flex flex-row w-3/4 items-center justify-center space-x-4'>
             <input type="text"
+            value={city}
+            onChange={(e) => {setCity(e.currentTarget.value)}}
             placeholder='Search..'
             className='text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize' />
-            <UilSearch size={25} className="text-white cursor-pointer transition ease-out hover:scale-125" />
-            <UilLocationPoint size={25} className="text-white cursor-pointer transition ease-out hover:scale-125" />
+            <UilSearch onClick={handleSearchClick} size={25} className="text-white cursor-pointer transition ease-out hover:scale-125" />
+            <UilLocationPoint onClick={handleLocationClick} size={25} className="text-white cursor-pointer transition ease-out hover:scale-125" />
         </div>
         <div className='flex flex-row w-1/4 items-center justify-center'>
-            <button name="metric" className='text-xl text-white font-light transition ease-out hover:scale-125'>°C</button>
+            <button onClick={handleUnitsChange} name="metric" className='text-xl text-white font-light transition ease-out hover:scale-125'>°C</button>
             <p className='text-xl text-white mx-1'>|</p>
-            <button name="imperial" className='text-xl text-white font-light transition ease-out hover:scale-125'>°F</button>
+            <button onClick={handleUnitsChange} name="imperial" className='text-xl text-white font-light transition ease-out hover:scale-125'>°F</button>
         </div>
     </div>
   )
--- src/Components/Topbuttons.jsx
+++ src/Components/Topbuttons.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Topbuttons() {
+function Topbuttons({setQuery}) {
     const cities = [
         {
             id:1,
@@ -26,7 +26,7 @@ function Topbuttons() {
   return (
     <div className='flex items-center justify-around  my-6'>
         {cities.map((city)=>(
-            <button key={city.id} className="text-white text-lg font-medium transition ease-out hover:scale-110">{city.title}</button>
+            <button key={city.id} onClick={()=>setQuery({q:city.title})} className="text-white text-lg font-medium transition ease-out hover:scale-110">{city.title}</button>
         ))}
     </div>
   )
