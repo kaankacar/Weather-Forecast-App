e108e81c4671e22a1189cc32e73d27b9d8d84c1f
kaankacar <kaankacar02@gmail.com>
2022-10-20 15:57:21 +0300

    constructing back end

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import './App.css';
+import Forecast from './Components/Forecast';
+import Input from './Components/Input';
+import TemperatureandDetails from './Components/TemperatureandDetails';
+import Timeandlocation from './Components/Timeandlocation';
 import Topbuttons from './Components/Topbuttons';
+import getFormattedWeatherData from './Services/Weatherservice';
 
 function App() {
+  const fetchWeather = async () => {
+    const data = await getFormattedWeatherData({q: "london"});
+    console.log(data);
+  };
+
+  fetchWeather();
+
   return (
     <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
       <Topbuttons />
+      <Input />
+
+      <Timeandlocation />
+      <TemperatureandDetails />
+      <Forecast title="hourly forecast" />
+      <Forecast title="daily forecast" />
     </div>
   );
 }
--- src/Components/Topbuttons.jsx
+++ src/Components/Topbuttons.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 
 function Topbuttons() {
+    const cities = [
+        {
+            id:1,
+            title:"London"
+        },
+        {
+            id:2,
+            title:"Sydney"
+        },
+        {
+            id:3,
+            title:"Tokyo"
+        },
+        {
+            id:4,
+            title:"Toronto"
+        },
+        {
+            id:5,
+            title:"Paris"
+        }
+    ]
   return (
-    <div>Topbuttons</div>
+    <div className='flex items-center justify-around  my-6'>
+        {cities.map((city)=>(
+            <button id={city.id} className="text-white text-lg font-medium">{city.title}</button>
+        ))}
+    </div>
   )
 }
 
